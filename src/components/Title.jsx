72a488804872af5dfab06353b83c8545de660f94
alaxgabriel71 <alaxgabriel71@gmail.com>
2024-07-26 15:24:09 -0300

    changing the photo path to correct build

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,7 +3,7 @@ import styles from './Title.module.css'
 function Title() {
     return(
         <div className={styles.Cape_container}>
-            <img src='../../public/alax.jpeg' alt="foto de alax"/>
+            <img src='./alax.png' alt="alax photo"/>
             <div className={styles.Title_container}>
                 <h1>Alax</h1>
                 <h2>Gabriel</h2>
